31d36116fc1ae613cf6311c25dbb938433b17f0f
caleb1248 <caleb@tremontschool.org>
2022-09-26 18:30:08 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Spanish flash cards in react</h1>
+    </>
+  )
+}
+
+export default App
